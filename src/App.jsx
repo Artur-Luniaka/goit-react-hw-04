cd1d9a5847cfa7195c489454df121e1c8b2dcaf8
Artur Luniaka <artur01049718@gmail.com>
2024-11-08 09:59:49 +0100

    added moreBtn and function loadMore with pagination

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,43 @@ import { fetchImages } from "./assets/api";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import { useState } from "react";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 
 const App = () => {
   const [images, setImages] = useState([]);
+  const [query, setQuery] = useState();
+  const [page, setPage] = useState(1);
+  const [visibleBtn, setVisibleBtn] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const query = e.target.elements.searchInput.value;
+    setQuery(query);
+    setPage(1);
     try {
       const result = await fetchImages(query, 1);
       setImages(result);
+      setVisibleBtn(true);
+      e.target.reset();
     } catch (error) {
       console.error(error);
+      setVisibleBtn(false);
+    }
+  };
+
+  const loadMoreImg = async () => {
+    try {
+      const nextPage = page + 1;
+      setPage(nextPage);
+      const newImages = await fetchImages(query, nextPage);
+      setImages((prevImages) => [...prevImages, ...newImages]);
+      if (newImages.length <= 0) {
+        setVisibleBtn(false);
+      } else {
+        setVisibleBtn(true);
+      }
+    } catch (error) {
+      console.error(error);
+      setVisibleBtn(false);
     }
   };
 
@@ -22,6 +48,7 @@ const App = () => {
     <>
       <SearchBar sendQuery={handleSubmit} />
       <ImageGallery cards={images} />
+      {visibleBtn ? <LoadMoreBtn onLoad={loadMoreImg} /> : ""}
     </>
   );
 };
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,13 @@
+import s from "./LoadMoreBtn.module.css";
+
+const LoadMoreBtn = ({ onLoad }) => {
+  return (
+    <div className={s.box}>
+      <button className={s.button} type="button" onClick={onLoad}>
+        Load more
+      </button>
+    </div>
+  );
+};
+
+export default LoadMoreBtn;
